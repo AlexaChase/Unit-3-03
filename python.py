ab35989ab0ffac42d06ad4558823e26bb6666780
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-18 14:14:05 +0000

    new changes

--- python.py
+++ python.py
@@ -1,5 +1,3 @@
-# python challenge 7 puzzle
-for number1 in range (10,100):
-    number2= int(number1) * 7
-    if number2== (str(number1)[1:]):
+for number1 in range(10,100):
+    if number1 == 7 * int(str(number1)[1:]):
         print(number1)
